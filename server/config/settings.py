102655865ef8790fca5b442c6ae9a4d3338a41ef
Yasser Messahli <yassermessahli0@gmail.com>
2024-11-21 23:24:06 +0100

    aaaaaa

--- server/config/settings.py
+++ server/config/settings.py
@@ -178,7 +178,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Enable this for production
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
